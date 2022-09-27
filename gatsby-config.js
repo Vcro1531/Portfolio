1e48b5ca104b79f40f6fb405420fda9790b8d6dd
KimJiMin <minmin4736@naver.com>
2022-09-27 17:05:22 +0900

    ⚡️ :: GraphQL Query gatsby 사용

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `사이트 데이터 테스트`,
+    author: `김지민`,
   },
   plugins: [
     {
