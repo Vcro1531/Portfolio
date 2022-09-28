cd14affc3192409ac81105826f7128e0ec0bb809
KimJiMin <minmin4736@naver.com>
2022-09-28 19:20:12 +0900

    ⚡️ :: Styled Component

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from "react"
 import { graphql } from "gatsby"
+import { Global, css } from "@emotion/react"
+import styled from "@emotion/styled"
 
 type InfoPageProps = {
   data: {
@@ -13,6 +15,32 @@ type InfoPageProps = {
   }
 }
 
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`
+
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`
+
+const Text1 = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`
+
+const Text2 = styled(div)(() => ({
+  fontSize: "15px",
+  color: "blue",
+}))
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -22,9 +50,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      {title}
-      {description}
-      {author}
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1>{description}</Text1>
+      <Text2>{author}</Text2>
     </div>
   )
 }
