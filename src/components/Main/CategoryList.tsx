597a50c25026bc12c5d2f16313db7dc2031a4161
KimJiMin <minmin4736@naver.com>
2022-10-11 16:10:23 +0900

    ⚡️ :: 카테고리 목록 컴포넌트

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,5 +1,16 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, ReactNode } from "react"
 import styled from "@emotion/styled"
+import { Link } from "gatsby"
+
+type CategoryItemProps = {
+  active: boolean
+}
+
+type GatsbyLinkProps = {
+  children: ReactNode
+  className?: string
+  to: string
+} & CategoryItemProps
 
 export type CategoryListProps = {
   selectedCategory: string
@@ -8,11 +19,42 @@ export type CategoryListProps = {
   }
 }
 
+const CategoryListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 767px;
+  margin: 100px auto 0;
+`
+
+const CategoryItem = styled(({ active, ...props }) => <Link {...props} />)`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? "800" : "400")};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
   selectedCategory,
   categoryList,
 }) {
-  return <div />
+  return (
+    <CategoryListWrapper>
+      {Object.entries(categoryList).map(([name, count]) => (
+        <CategoryItem
+          to={`/?category=${name}`}
+          active={name === selectedCategory}
+          key={name}
+        >
+          #{name}({count})
+        </CategoryItem>
+      ))}
+    </CategoryListWrapper>
+  )
 }
 
 export default CategoryList
