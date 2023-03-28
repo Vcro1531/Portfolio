3fe43db507f285d4454e3c58be1b4f40c61a6c6b
Vcro1531 <jiminkim0520@gmail.com>
2023-03-28 21:38:45 +0900

    ⚡️ :: theme 기초 세팅

---
+++ src/style/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    black: "#000000",
+    white: "#FFFFFF",
+    primary: "#006EFF",
+    check: "#04DF00",
+    error: "#DC3035",
+    main: "",
+  },
+  font: {
+    noto: "Noto Sans",
+  },
+};
+
+export default theme;
